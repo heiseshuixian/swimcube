7aa7fd08df37fac759f59a1b58a94d40ee483788
Rg <rg@miugodigital.com>
2017-05-20 20:31:26 +0800

    [Update]小鱼加上碰撞

---
+++ SwimCube/Assets/Scripts/playerhpbar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class playerhpbar : MonoBehaviour {
+	private Image[] hearts;
+	// Use this for initialization
+	void Start () {
+		hearts = GetComponentsInChildren<Image> ();
+	}
+	public void setValue(float value){
+		if(value>1){value = 1;}
+		if(value<0){value = 0;}
+		int light =(int) Mathf.Floor(hearts.Length*(value));
+		float helf = hearts.Length*(value) -light;
+		int helfIndex=light;
+		for(int index =0;index<hearts.Length;index++){
+			if (index <= light-1) {
+				hearts [index].fillAmount = 1;
+			} else if (index == helfIndex) {
+				hearts [index].fillAmount = helf;
+			} else {
+				hearts [index].fillAmount = 0;
+			}
+		}
+	}
+}
